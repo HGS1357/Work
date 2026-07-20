7a954c38a9ee5660b4a30081eae447ee9874ec18
HGS1357 <hdh26012@gmail.com>
2026-07-20 14:28:59 +0900

    Day1

---
+++ tast.c
@@ -0,0 +1,10 @@
+/*#include <stdio.h>
+#include <stdlib.h>
+
+/*int main(void)             // 메인함수 이다 함수는 이름 형태가 함수이다 
+  {                        // 메인 함수는 프로그램에 꼭 1개가 존재하여야 한다. 
+   printf("be happy!");   // printf도 함수이다. 왜? ()가 있으니까 
+   system("pause");       //system
+   return 0;   
+}*/
+ 
---
+++ tast2.c
@@ -0,0 +1,8 @@
+/*#include <stdio.h>
+
+
+int main()
+{
+	printf("한글 출력 쌉가능?");
+	return 0;
+}*/
